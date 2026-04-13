70d73f5222ac484427ae9b7bc236ce93784e4176
RenChu Wang <patrick1031wang@gmail.com>
2026-04-13 12:32:26 +0800

    format function

--- src/aioway/_common/fmt.py
+++ src/aioway/_common/fmt.py
@@ -1,17 +1,61 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
 import dataclasses as dcls
+import functools
 import typing
 from collections import abc as cabc
+
+from .typing import is_dict_of_str_to
 
 __all__ = ["format_function", "dcls_no_eq", "dcls_no_eq_no_repr"]
 
 
-def format_function(
-    func: cabc.Callable, *args: typing.Any, **kwargs: typing.Any
-) -> str:
-    "Format the function into readable string, mimicking signature in python."
+@dcls.dataclass(frozen=True)
+class _FuncFormat:
+    """
+    A custom object that lazily formats the function, to avoid unecessary computation.
+    """
 
+    func: cabc.Callable[..., typing.Any]
+    "The function. Must be callable."
+
+    args: tuple[typing.Any, ...]
+    kwargs: dict[str, typing.Any]
+
+    def __post_init__(self) -> None:
+        if not callable(self.func):
+            raise ValueError("Cannot format function that isn't callable.")
+
+        if not isinstance(self.args, tuple):
+            raise ValueError(f"Args: {self.args!r} is not valid.")
+
+        if not is_dict_of_str_to(object)(self.kwargs):
+            raise ValueError(f"Kwargs: {self.kwargs!r} is not valid.")
+
+    @typing.override
+    def __str__(self) -> str:
+        return self.__string
+
+    @functools.cached_property
+    def __string(self) -> str:
+        return _format_function_as_str(self.func, *self.args, **self.kwargs)
+
+
+def format_function(
+    func: cabc.Callable[..., typing.Any], *args: typing.Any, **kwargs: typing.Any
+):
+    """
+    Formats the function into readable string, mimicking signature in python.
+
+    Returns a custom object that lazily formats the function call to string.
+    """
+
+    return _FuncFormat(func, args, kwargs)
+
+
+def _format_function_as_str(
+    func: cabc.Callable[..., typing.Any], *args: typing.Any, **kwargs: typing.Any
+) -> str:
     args_builder: list[str] = []
 
     # Add positional arguments.
--- src/aioway/fn/dispatch.py
+++ src/aioway/fn/dispatch.py
@@ -3,6 +3,7 @@
 "This module contains utilities that uses the `__torch_dispatch__` mode."
 
 import contextlib as ctxl
+import logging
 import typing
 
 import torch
@@ -12,6 +13,23 @@ from torch.utils import _python_dispatch as pyd
 from aioway._common import format_function
 
 __all__ = ["print_torch_dispatch"]
+
+LOGGER = logging.getLogger(__name__)
+
+
+class _StoreDispatchMode(pyd.TorchDispatchMode):
+    def __torch_dispatch__(
+        self,
+        func: _ops.OpOverload,
+        types: tuple[type[torch.Tensor], ...],
+        args: tuple[typing.Any, ...] = (),
+        kwargs: dict[str, typing.Any] | None = None,
+    ):
+        kwargs = kwargs or {}
+        print(format_function(func, *args, **kwargs))
+        result = func(*args, **kwargs)
+        print(result, id(result))
+        return result
 
 
 class _PrintDispatchMode(pyd.TorchDispatchMode):
@@ -24,9 +42,26 @@ class _PrintDispatchMode(pyd.TorchDispatchMode):
         kwargs: dict[str, typing.Any] | None = None,
     ):
         kwargs = kwargs or {}
-        print(format_function(func, *args, **kwargs))
         result = func(*args, **kwargs)
-        print(result, id(result))
+        func_call = format_function(func, *args, **kwargs)
+        print(f"{func_call} -> {result!r}")
+        return result
+
+
+class _LogDispatchMode(pyd.TorchDispatchMode):
+
+    def __torch_dispatch__(
+        self,
+        func: _ops.OpOverload,
+        types: tuple[type[torch.Tensor], ...],
+        args: tuple[typing.Any, ...] = (),
+        kwargs: dict[str, typing.Any] | None = None,
+    ):
+        kwargs = kwargs or {}
+        result = func(*args, **kwargs)
+        if LOGGER.isEnabledFor(logging.DEBUG):
+            func_call = format_function(func, *args, **kwargs)
+            LOGGER.debug(f"%s -> %s", func_call, result)
         return result
 
 
