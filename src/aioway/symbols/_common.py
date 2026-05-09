34c3931d1cd4e83dcd458506fa61746ab5c25a6b
RenChu Wang <patrick1031wang@gmail.com>
2026-05-08 17:57:41 -0700

    🧨 Remove symbols, which is now unused. (#216)

--- src/aioway/symbols/_common.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) AIoWay Authors - All Rights Reserved
-
-import dataclasses as dcls
-import typing
-
-
-@typing.dataclass_transform(eq_default=False)
-def symbol_dataclass[T: type](cls: T) -> T:
-    return typing.cast(T, dcls.dataclass(eq=False)(cls))
