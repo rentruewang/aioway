ea2a8f81eb9a74e3072d62f2cbb64650dbd740c7
RenChu Wang <patrick1031wang@gmail.com>
2026-05-14 13:30:19 -0700

    📓 Add notebooks to the repository (#232)

---
+++ notebooks/hello.py
@@ -0,0 +1,23 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Hello
+
+# %% [markdown]
+# ### This is the first notebook
+
+# %%
+print("hello world")
