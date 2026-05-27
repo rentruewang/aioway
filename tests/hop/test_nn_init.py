c193e84ddf8538d6780012414cce72a613c71b76
RenChu Wang <patrick1031wang@gmail.com>
2026-05-27 16:58:48 -0700

    tests ok

--- tests/hop/test_nn_init.py
+++ tests/hop/test_nn_init.py
@@ -7,12 +7,21 @@ from aioway.hop import NnInit, Sequential, find_nn_init
 from aioway.modes import NnInitFn
 
 
-def test_nn_init(layer_nn_init: NnInit):
+def test_layer_nn_init(layer_nn_init: NnInit):
     assert isinstance(layer_nn_init, NnInit)
 
 
 def test_layer_nn_init_module(layer_nn_init: NnInit):
     module = layer_nn_init()
+    assert isinstance(module, nn.Module)
+
+
+def test_loss_nn_init(loss_nn_init: NnInit):
+    assert isinstance(loss_nn_init, NnInit)
+
+
+def test_loss_nn_init_module(loss_nn_init: NnInit):
+    module = loss_nn_init()
     assert isinstance(module, nn.Module)
 
 
