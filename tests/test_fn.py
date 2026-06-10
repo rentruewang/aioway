4b4169fe768880d7284300f2f68c54dccdac773f
RenChu Wang <patrick1031wang@gmail.com>
2026-06-09 21:10:55 -0700

    hop

--- tests/test_fn.py
+++ tests/test_fn.py
@@ -5,7 +5,6 @@ import pytest
 from aioway._fn import Fn, TensorInput, TorchThunk
 from aioway.ctx import HistTensorGraph
 from aioway.fate import Fate, FateFn
-from aioway.hop import Hop
 from aioway.modes import NnFwdFn, NnInitFn, TorchDispFn, TorchFuncFn
 
 
@@ -17,7 +16,6 @@ def _fn_cls():
     yield NnFwdFn
 
     yield Fate
-    yield Hop
 
 
 def _input_cls():
