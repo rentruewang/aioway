cd85cf6ea0158e3baac6f886fca2a356f1a55a77
RenChu Wang <patrick1031wang@gmail.com>
2026-06-10 18:53:39 -0700

    frames go under io

--- src/aioway/io/__init__.py
+++ src/aioway/io/__init__.py
@@ -1,5 +1,3 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
-from ._frames import *
-from .chunks import *
-from .hop import *
+from .frames import *
--- src/aioway/io/frames/__init__.py
+++ src/aioway/io/frames/__init__.py
@@ -1,1 +1,5 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
+
+from ._frames import *
+from .chunks import *
+from .hop import *
