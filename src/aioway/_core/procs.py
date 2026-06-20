97922d85135297491b3604d9ea08e21afeb22e34
RenChu Wang <patrick1031wang@gmail.com>
2026-06-19 21:26:29 -0700

    fmt

--- src/aioway/_core/procs.py
+++ src/aioway/_core/procs.py
@@ -7,7 +7,7 @@ import contextlib as ctxl
 import typing
 from collections import abc as cabc
 
-from aioway._core import sample_mode, current_sample_mode
+from aioway._core import current_sample_mode, sample_mode
 from aioway._utils import (
     AnyDict,
     torch_fake_mode,
