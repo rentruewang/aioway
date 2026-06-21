eeccccdfebd96c162c0b49426cd92d1e1949ddd2
RenChu Wang <patrick1031wang@gmail.com>
2026-06-20 19:18:57 -0700

    Detect stages in the built graph.
    Fix #296

--- src/aioway/_core/procs.py
+++ src/aioway/_core/procs.py
@@ -8,10 +8,7 @@ import typing
 from collections import abc as cabc
 
 from aioway._core import current_sample_mode, sample_mode
-from aioway._utils import (
-    AnyDict,
-    torch_fake_mode,
-)
+from aioway._utils import AnyDict, torch_fake_mode
 
 from .iters import Iter
 
