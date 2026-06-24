0fff7adaa7f0dd5af19a4d63292110dd97e546b8
RenChu Wang <patrick1031wang@gmail.com>
2026-06-24 00:46:14 -0700

    updat

--- src/aioway/_iters/iters.py
+++ src/aioway/_iters/iters.py
@@ -203,12 +203,13 @@ class StructIter(Iter[typing.Any]):
 
         # Inside the structure, call `next` on all `IterProc`s, until `StopIteration`.
         while True:
-            try:
-                # Turn on the cache, because the dependencies can still be DAGs.
-                with iter_cache_on():
+            # Turn on the cache, because the dependencies can still be DAGs.
+            with iter_cache_on():
+
+                try:
                     yield decomp_replace(struct_of_iter, next_it)
-            except StopIteration:
-                return
+                except StopIteration:
+                    return
 
 
 @node_dcls
