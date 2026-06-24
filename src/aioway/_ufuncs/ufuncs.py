d197f31300291413014a53cd1112b37ae14564ea
RenChu Wang <patrick1031wang@gmail.com>
2026-06-23 23:33:40 -0700

    not working yet

--- src/aioway/_ufuncs/ufuncs.py
+++ src/aioway/_ufuncs/ufuncs.py
@@ -1,34 +1,63 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
 import abc
+import inspect
 import typing
-from collections import abc as cabc
 
-from aioway._iters import GraphNode, Iter, node_dcls
-from aioway._thunks import Thunk
-
-__all__ = ["UFunc", "LazyThunk"]
+__all__ = ["UFunc"]
 
 
-@node_dcls
-class LazyThunk[T](Thunk[T], GraphNode, abc.ABC):
-    """
-    The `Thunk` that lazily invokes its dependencies, with back tracing info.
-    """
-
-
-@typing.runtime_checkable
-class UFunc[T = typing.Any](typing.Protocol):
+class UFunc(abc.ABC):
     """
     `UFunc`, inspired by `numpy`, stands for universal functions,
     and are the building blocks of other APIs (like thunks and iters).
     """
 
-    __call__: cabc.Callable[..., T]
-    "A `UFunc` is callable (imperative)."
+    def __str__(self) -> str:
+        "Repr is equivalent to object + signature."
+        return object.__repr__(self) + str(self.__signature__)
 
-    thunk: cabc.Callable[..., LazyThunk[T]]
-    "A `UFunc` takes thunks and tranform it into other thunks."
+    def __call__(self, *args, **kwargs) -> typing.Any:
+        """
+        A `UFunc` is callable (imperative).
+        The calling logic is defined in `forward`.
 
-    iter: cabc.Callable[..., Iter[T]]
-    "A `UFunc` takes iterators and tranform it into other iterators."
+        The signature of it would be checked prior to calling.
+        """
+
+        self._check_signature(*args, **kwargs)
+        return self.forward(*args, **kwargs)
+
+    def _repr(self) -> str:
+        "The `__repr__` of the instance itself, excluding signature."
+        return object.__repr__(self)
+
+    @abc.abstractmethod
+    @typing.no_type_check
+    def forward(self, *args, **kwargs) -> T:
+        raise NotImplementedError
+
+    def thunk(self, *args, **kwargs) -> typing.Any:
+        "A `UFunc` takes thunks and tranform it into other thunks."
+        self._check_signature(*args, **kwargs)
+        raise NotImplementedError
+
+    def iter(self, *args, **kwargs) -> typing.Any:
+        "A `UFunc` takes iterators and tranform it into other iterators."
+        self._check_signature(*args, **kwargs)
+        raise NotImplementedError
+
+    @property
+    def __signature__(self) -> inspect.Signature:
+        "The signature of the `UFunc`."
+        return inspect.signature(self.forward)
+
+    def _check_signature(self, *args, **kwargs) -> None:
+        # This only checks the signature names, not types,
+        # so it's perfect for our usage, because `__call__`, `thunk`, `iter`
+        # all share the same signature but with different types.
+
+        try:
+            _ = self.__signature__.bind(*args, **kwargs)
+        except TypeError as te:
+            raise TypeError(f"{self!r} gets a bad input.") from te
--- src/aioway/torch/nn/ufuncs.py
+++ src/aioway/torch/nn/ufuncs.py
@@ -1,5 +1,7 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
+"`UFunc` implementation for `torch.nn`."
+
 import abc
 import dataclasses as dcls
 import typing
@@ -9,7 +11,7 @@ import torch
 from torch import nn
 
 from aioway._iters import TensorIter, node_dcls
-from aioway._ufuncs import LazyThunk, UFunc
+from aioway._ufuncs import UFunc
 
 if typing.TYPE_CHECKING:
     from aioway.torch.nn import NnInit
@@ -43,7 +45,7 @@ class NnUFunc(UFunc, abc.ABC):
 class NnLossUFunc(NnUFunc):
 
     @typing.override
-    def __call__(self, input: torch.Tensor, target: torch.Tensor, /) -> torch.Tensor:
+    def forward(self, input: torch.Tensor, target: torch.Tensor, /) -> torch.Tensor:
         return self._module(input, target)
 
     @typing.override
@@ -54,31 +56,14 @@ class NnLossUFunc(NnUFunc):
         assert isinstance(self.module, self.nn_init.NN)
 
     @typing.override
-    def thunk(
-        self, input: LazyThunk[torch.Tensor], target: LazyThunk[torch.Tensor], /
-    ) -> LazyThunk[torch.Tensor]:
-        return NnLossThunk(self.module, input, target)
-
-    @typing.override
     def iter(self, input: TensorIter, target: TensorIter) -> NnLossIter:
         return NnLossIter(self.nn_init, self.module, input, target)
 
 
 @typing.final
-@dcls.dataclass
-class NnLossThunk(LazyThunk[torch.Tensor]):
-    module: nn.Module
-    input: LazyThunk
-    target: LazyThunk
-
-    def __call__(self):
-        return self.module(self.input, self.target)
-
-
-@typing.final
 class NnLayerUFunc(NnUFunc):
     @typing.override
-    def __call__(self, input: torch.Tensor, /) -> torch.Tensor:
+    def forward(self, input: torch.Tensor, /) -> torch.Tensor:
         return self._module(input)
 
     @typing.override
@@ -89,22 +74,8 @@ class NnLayerUFunc(NnUFunc):
         assert isinstance(self.module, self.nn_init.NN)
 
     @typing.override
-    def thunk(self, input: LazyThunk[torch.Tensor], /) -> LazyThunk[torch.Tensor]:
-        return NnLayerThunk(self.module, input)
-
-    @typing.override
     def iter(self, input: TensorIter) -> NnLayerIter:
         return NnLayerIter(self.nn_init, self.module, input)
-
-
-@typing.final
-@dcls.dataclass
-class NnLayerThunk(LazyThunk):
-    module: nn.Module
-    input: LazyThunk
-
-    def __call__(self):
-        return self.module(self.input)
 
 
 @node_dcls
