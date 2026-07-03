61d193d6748a9dea435ee78096854b4872a179e0
RenChu Wang <patrick1031wang@gmail.com>
2026-07-02 19:48:53 -0700

    update imports

--- src/aioway/__init__.py
+++ src/aioway/__init__.py
@@ -2,21 +2,23 @@
 
 "The global modules."
 
-from ._api import *
-from ._costs import *
-from ._iters import *
-from ._sess import *
-from ._thunks import *
-from ._ufuncs import *
-from ._utils import *
-from .api import *
-from .codecs import *
-from .dsets import *
-from .emits import *
-from .envs import *
-from .errors import *
-from .indices import *
-from .modes import *
-from .relalg import *
-from .spaces import *
-from .torch import *
+from . import (
+    _api,
+    _costs,
+    _iters,
+    _sess,
+    _thunks,
+    _ufuncs,
+    _utils,
+    api,
+    codecs,
+    dsets,
+    emits,
+    envs,
+    errors,
+    indices,
+    modes,
+    relalg,
+    spaces,
+    torch,
+)
