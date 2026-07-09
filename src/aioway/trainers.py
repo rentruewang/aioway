ce14b3c88d5f1ff3e45f49ceb7702f3f16fa32cb
RenChu Wang <patrick1031wang@gmail.com>
2026-07-08 22:41:46 -0700

    🧑‍💻 Hacky `fit` / `predict` api (#393)

---
+++ src/aioway/trainers.py
@@ -0,0 +1,38 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+
+from torch import nn, optim
+
+from aioway._ufuncs import BuiltUFunc, UFunc
+from aioway.emits import emit
+from aioway.io import Dset
+from aioway.relalg import LoaderOpt
+from aioway.torch.optim import OptimizerUFunc
+
+__all__ = ["Trainer"]
+
+
+class Trainer:
+    def __init__(self, loss_func: nn.Module) -> None:
+        self._loss_func = loss_func
+        self._ufunc: UFunc | None = None
+
+    def fit(self, x: Dset, y: Dset, loader_opt: LoaderOpt = LoaderOpt()):
+        self._ufunc = next(emit(x.space, y.space))
+        assert isinstance(self.ufunc, BuiltUFunc), self.ufunc
+        optimizer = OptimizerUFunc(optim.AdamW(self.ufunc.parameters()))
+
+        for batch_x, batch_y in zip(x(loader_opt), y(loader_opt)):
+            out = self.ufunc(batch_x)
+            loss = self._loss_func(out, batch_y)
+            optimizer(loss)
+            yield loss
+
+    def predict(self, x: Dset, loader_opt: LoaderOpt = LoaderOpt()):
+        for batch_x in x(loader_opt):
+            yield self.ufunc(batch_x)
+
+    @property
+    def ufunc(self) -> UFunc:
+        assert self._ufunc is not None
+        return self._ufunc
---
+++ tests/test_trainers.py
@@ -0,0 +1,60 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+import dataclasses as dcls
+
+import pytest
+import torch
+from torch import nn
+
+from aioway.emits import MlpEmitter
+from aioway.io import TensorFrame
+from aioway.spaces import ShapeSpace
+from aioway.trainers import Trainer
+
+
+@dcls.dataclass(frozen=True)
+class _TensorFrame(TensorFrame):
+    tensor: torch.Tensor
+
+    def __len__(self):
+        return len(self.tensor)
+
+    def __getitem__(self, idx):
+        return self.tensor[idx]
+
+    def __getitems__(self, idx):
+        return list(self.tensor[idx])
+
+    @property
+    def space(self):
+        return ShapeSpace(self.tensor.shape[1:])
+
+
+@pytest.fixture
+def trainer():
+    return Trainer(nn.MSELoss())
+
+
+@pytest.fixture
+def x():
+    return _TensorFrame(torch.randn(100, 13))
+
+
+@pytest.fixture
+def y():
+    return _TensorFrame(torch.randn(100, 11))
+
+
+@pytest.fixture(autouse=True)
+def mlp_emitter():
+    with MlpEmitter([17, 19]).consider():
+        yield
+
+
+def test_trainer_fit(trainer, x, y):
+    list(trainer.fit(x, y))
+
+
+def test_trainer_fit_predict(trainer, x, y):
+    list(trainer.fit(x, y))
+    list(trainer.predict(x))
