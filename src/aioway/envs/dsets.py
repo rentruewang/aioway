10d6f0ed40e8593e3507fb99f3e974f90ecbdac7
RenChu Wang <patrick1031wang@gmail.com>
2026-08-02 09:21:39 -0700

    frame done

--- src/aioway/envs/dsets.py
+++ src/aioway/envs/dsets.py
@@ -34,3 +34,39 @@ class IteratorEnv[T](Env[T]):
     @typing.override
     def clone(self) -> typing.Self:
         return copy.deepcopy(self)
+
+
+@typing.final
+class ListEnv[T](Env[T]):
+    """
+    This kind of `Env` stores a list of inputs and yield them one by one.
+    Each should be a
+    """
+
+    def __init__(self, sequence: cabc.Sequence[T], space: DataSpace) -> None:
+        self._idx = 0
+        self._sequence = sequence
+        self._space = space
+
+    @typing.override
+    def _get_next(self) -> T:
+        try:
+            result = self._sequence[self._idx]
+        except IndexError:
+            raise StopIteration
+
+        self._idx += 1
+        return result
+
+    @property
+    @typing.override
+    def observ_space(self) -> DataSpace:
+        return self._space
+
+    @property
+    def idx(self) -> int:
+        return self._idx
+
+    def clone(self) -> typing.Self:
+        # Does not need deepcopy!
+        return copy.copy(self)
