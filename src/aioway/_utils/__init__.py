542f4db03e18c5d1615f8dc5b6e1afe499848ec3
RenChu Wang <patrick1031wang@gmail.com>
2026-08-02 21:39:15 -0700

    move tdict out

--- src/aioway/_utils/__init__.py
+++ src/aioway/_utils/__init__.py
@@ -10,7 +10,6 @@ from .platforms import *
 from .profs import *
 from .renders import *
 from .signs import *
-from .tdicts import *
 from .tensors import *
 from .types import *
 from .typing import *
