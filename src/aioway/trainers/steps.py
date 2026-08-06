a9f39019c99c76438aea9ad89d62e37daaafd8d5
RenChu Wang <patrick1031wang@gmail.com>
2026-08-05 17:58:31 -0700

    update

--- src/aioway/trainers/steps.py
+++ src/aioway/trainers/steps.py
@@ -1,24 +1,43 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
+import typing
 import abc
+from collections import abc as cabc
 
+import torch
 from torch import nn
-import tensordict as td
-import torch, typing
+from torch.utils import data
+
+__all__ = ["TrainLoss"]
 
 
-__all__ = ["TrainStep"]
+class TrainLoss[T](nn.Module, abc.ABC):
+    """
+    The training step function.
 
+    It is an `nn.Module` s.t. we can directly use optimizers on it.
+    """
 
-class TrainStep(abc.ABC):
-    "The training step function."
+    BATCH_CLS: typing.ClassVar[type] = object
+    "The batching class to use."
 
-    BATCH_CLS: type = object
+    module: nn.Module
+    "The module to optimize."
 
-    def __call__(self, batch: typing.Any) -> torch.Tensor:
+    def __call__(self, batch: T) -> torch.Tensor:
         assert isinstance(batch, self.BATCH_CLS)
         return self.forward(batch)
 
     @abc.abstractmethod
-    def forward(self, batch: typing.Any) -> torch.Tensor:
+    def forward(self, batch: T) -> torch.Tensor:
         raise NotImplementedError
+
+    def for_data_loader(
+        self, loader: data.DataLoader[T]
+    ) -> cabc.Generator[torch.Tensor]:
+        """
+        Compute the losses over an entire data loader.
+        """
+
+        for batch in loader:
+            yield self(batch)
