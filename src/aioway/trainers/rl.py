1c3acc782316866cfe10e96754f833abef7c6eb8
RenChu Wang <patrick1031wang@gmail.com>
2026-08-05 22:48:43 -0700

    DQN

---
+++ src/aioway/trainers/rl.py
@@ -0,0 +1,40 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+from debugpy.launcher.debuggee import process
+from torchrl import envs as trl_envs
+from torchrl import collectors as trl_cols
+import contextlib as ctxl
+
+from torch import nn
+
+from collections import abc as cabc
+
+__all__ = ["vec_env", "collector"]
+
+
+@ctxl.contextmanager
+def vec_env(name: str, processes: int) -> cabc.Generator[trl_envs.ParallelEnv]:
+    env = trl_envs.GymEnv(name)
+    parallel = trl_envs.ParallelEnv(processes, lambda: env)
+
+    try:
+        yield parallel
+    finally:
+        parallel.close()
+
+
+@ctxl.contextmanager
+def collector(
+    env: trl_envs.EnvBase, policy: nn.Module, frames_per_batch: int, total_frames: int
+) -> cabc.Generator[trl_cols.Collector]:
+    collector = trl_cols.Collector(
+        create_env_fn=env,
+        policy=policy,
+        frames_per_batch=frames_per_batch,
+        total_frames=total_frames,
+    )
+
+    try:
+        yield collector
+    finally:
+        collector.shutdown()
