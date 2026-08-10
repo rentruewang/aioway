34d11c0bdad9c5c961557a3d7e77aa64d0e92a64
RenChu Wang <patrick1031wang@gmail.com>
2026-08-10 00:32:05 -0700

    😐 Norms (#459)
    Fix #25

---
+++ src/aioway/emits/norms.py
@@ -0,0 +1,52 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+"Normalization layers."
+
+import typing
+
+from torch import nn
+from torchrl import data as rldata
+
+from .emitters import Emitter, emitter_dcls, emitter_function
+
+__all__ = ["NormEmitter", "NormType", "layer_norm_emitter"]
+
+type NormType = typing.Literal["instance", "batch"]
+
+
+@emitter_dcls
+class NormEmitter(Emitter):
+    norm_type: NormType
+    "The type of normalization layer."
+
+    def __call__(
+        self, observ: rldata.TensorSpec, action: rldata.TensorSpec
+    ) -> nn.Module:
+        if not isinstance(observ, rldata.Unbounded):
+            return NotImplemented
+
+        if not isinstance(action, rldata.Unbounded):
+            return NotImplemented
+
+        if observ != action:
+            return NotImplemented
+
+        return self._get_norm(observ.ndim - 1, observ.shape[0])
+
+    def _get_norm(self, non_channel_ndim: int, num_features: int) -> nn.Module:
+        klass = getattr(nn, f"{self.norm_type.capitalize()}Norm{non_channel_ndim}d")
+        return klass(num_features=num_features)
+
+
+@emitter_function
+def layer_norm_emitter(observ, action):
+    if not isinstance(observ, rldata.Unbounded):
+        return NotImplemented
+
+    if not isinstance(action, rldata.Unbounded):
+        return NotImplemented
+
+    if observ != action:
+        return NotImplemented
+
+    return nn.LayerNorm(observ.shape)
---
+++ tests/emits/test_norms.py
@@ -0,0 +1,55 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+import typing
+
+import pytest
+
+from aioway._specs import unbounded_box_spec
+from aioway.emits import NormEmitter, NormType, emit_one, layer_norm_emitter
+
+
+@pytest.fixture(params=typing.get_args(NormType.__value__))
+def norm(request: pytest.FixtureRequest):
+    return request.param
+
+
+@pytest.fixture(params=[1, 2, 3])
+def ndim(request: pytest.FixtureRequest):
+    return request.param
+
+
+@pytest.fixture(params=[11, 13, 17])
+def num_features(request: pytest.FixtureRequest):
+    return request.param
+
+
+@pytest.fixture
+def batch_inst_norm_emitter(norm):
+    with NormEmitter(norm).consider():
+        yield
+
+
+@pytest.fixture
+def layer_norm():
+    with layer_norm_emitter.consider():
+        yield
+
+
+@pytest.fixture
+def spec(ndim, num_features):
+    shape = list(range(5, 5 + ndim))
+    return unbounded_box_spec(shape=[num_features, *shape])
+
+
+def test_batch_inst_norm(batch_inst_norm_emitter, spec):
+    mod = emit_one(spec, spec)
+    input = spec.sample([13])
+    output = mod(input)
+    assert output.shape == input.shape
+
+
+def test_layer_norm(layer_norm, spec):
+    mod = emit_one(spec, spec)
+    input = spec.sample([13])
+    output = mod(input)
+    assert output.shape == input.shape
