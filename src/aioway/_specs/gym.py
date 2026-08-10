583c0147be9319d0a37d9f61abb3997bc0414aec
RenChu Wang <patrick1031wang@gmail.com>
2026-08-09 20:35:25 -0700

    bounded

---
+++ src/aioway/_specs/gym.py
@@ -0,0 +1,80 @@
+# Copyright (c) AIoWay Authors - All Rights Reserved
+
+import numpy as np
+import torch
+from numpy import typing as npt
+from torchrl import data as rldata
+
+from aioway._torch import Device, DeviceLike, DType, DTypeLike, Shape, ShapeLike
+
+__all__ = ["scalar_box_spec", "array_box_spec"]
+
+
+def scalar_box_spec(
+    low: float, high: float, *, shape: ShapeLike, dtype: DTypeLike, device: DeviceLike
+) -> rldata.Bounded:
+    """
+    A continuous tensor bounded elementwise by `low` and `high`. Scalar version.
+
+    Args:
+        low: The scalar lower bound.
+        high: The scalar higher bound.
+        shape: The shape of the tensors.
+        dtype: The dtype of the values.
+        device: The device of the tensors.
+
+    Returns:
+        A `Bounded` spec.
+    """
+
+    return rldata.Bounded(
+        low=low,
+        high=high,
+        shape=Shape.parse(shape).torch(),
+        device=Device.parse(device).torch(),
+        dtype=DType.parse(dtype).torch(),
+    )
+
+
+def array_box_spec(
+    low: npt.ArrayLike,
+    high: npt.ArrayLike,
+    *,
+    shape: ShapeLike,
+    dtype: DTypeLike,
+    device: DeviceLike,
+) -> rldata.Bounded:
+    """
+    A continuous tensor bounded elementwise by `low` and `high`.
+    Here each element of the shape can have different bounds.
+
+    Args:
+        low: The array lower bound.
+        high: The array higher bound.
+        shape: The shape of the tensors. Must match the `low` and `high` shapes.
+        dtype: The dtype of the values. Must match the `low` and `high` shapes.
+        device: The device of the tensors. Must match the `low` and `high` shapes.
+
+    Returns:
+        A `Bounded` spec.
+    """
+
+    torch_device = Device.parse(device).torch()
+    torch_shape = Shape.parse(shape).torch()
+    torch_dtype = DType.parse(dtype).torch()
+
+    low_tensor = torch.from_numpy(np.asarray(low))
+    high_tensor = torch.from_numpy(np.asarray(high))
+
+    if low_tensor.shape != torch_shape:
+        raise ValueError(f"{low_tensor.shape=} does not match {shape=}.")
+    if high_tensor.shape != torch_shape:
+        raise ValueError(f"{high_tensor.shape=} does not match {shape=}.")
+
+    return rldata.Bounded(
+        low=low_tensor,
+        high=high_tensor,
+        shape=torch_shape,
+        device=torch_device,
+        dtype=torch_dtype,
+    )
