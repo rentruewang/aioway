e6718a27f9600a4bffdeef07514d52bcc6bff80f
RenChu Wang <patrick1031wang@gmail.com>
2026-08-22 18:48:58 -0700

    added the adds

--- src/aioway/app/entrypoint.py
+++ src/aioway/app/entrypoint.py
@@ -3,7 +3,7 @@
 from aioway.dsets import Dset
 from aioway.tspecs import TSpec
 
-__all__ = ["add_source", "add_sink"]
+__all__ = ["add_input", "add_output", "add_label"]
 
 _INPUT_DATASETS: dict[str, Dset] = {}
 "The input datasets."
@@ -11,28 +11,38 @@ _INPUT_DATASETS: dict[str, Dset] = {}
 _OUTPUT_SPECS: dict[str, TSpec] = {}
 "The output shapes."
 
+_LABEL_DATASETS: dict[str, Dset] = {}
+"The label dataset."
 
-def add_source(name: str, dset: Dset) -> None:
+
+def add_input(name: str, dset: Dset) -> None:
     """
     Add an input source.
     """
 
-    if _already_has_name(name):
-        raise KeyError(f"{name=} is already used.")
+    if name in _INPUT_DATASETS:
+        raise KeyError(f"{name=} is already used by another input dataset.")
 
     _INPUT_DATASETS[name] = dset
 
 
-def add_sink(name: str, spec: TSpec) -> None:
+def add_output(name: str, spec: TSpec) -> None:
     """
     Add an output sink.
     """
 
-    if _already_has_name(name):
-        raise KeyError(f"{name=} is already used")
+    if name in _OUTPUT_SPECS:
+        raise KeyError(f"{name=} is already used by another output sink.")
 
     _OUTPUT_SPECS[name] = spec
 
 
-def _already_has_name(name: str):
-    return name in _INPUT_DATASETS or name in _OUTPUT_SPECS
+def add_label(name: str, dset: Dset) -> None:
+    """
+    Add a training label dataset.
+    """
+
+    if name in _LABEL_DATASETS:
+        raise KeyError(f"{name=} is already used by another label dataset.")
+
+    _LABEL_DATASETS[name] = dset
