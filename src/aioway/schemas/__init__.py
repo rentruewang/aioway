19f308903563edc86f204cf7fcdcbd79d88c115b
RenChu Wang <patrick1031wang@gmail.com>
2026-08-25 22:22:51 -0700

    add backw

--- src/aioway/schemas/__init__.py
+++ src/aioway/schemas/__init__.py
@@ -4,6 +4,7 @@ from .attrs import *
 from .devices import *
 from .dtypes import *
 from .layouts import *
+from .modules import *
 from .renders import *
 from .schemas import *
 from .shapes import *
