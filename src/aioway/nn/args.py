362a537e49ee29a8e47d8ed21033bc0c07f5d9a9
RenChu Wang <patrick1031wang@gmail.com>
2026-09-07 22:00:55 -0700

    update

--- src/aioway/nn/args.py
+++ src/aioway/nn/args.py
@@ -1,5 +1,6 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
+import torch
 import inspect
 import typing
 import tensordict as td
@@ -18,20 +19,40 @@ class ArgsTSpec(tspecs.Composite):
     """
 
 
+class Invoker(typing.Protocol):
+    def __call__(self, module: nn.Module) -> typing.Any: ...
+
+
 @dcls.dataclass(frozen=True)
 class NnArgs:
     "The tensordict that marks something"
 
-    tdict: td.TensorDict | td.TensorClass | cabc.Mapping[str, typing.Any]
+    arguments: typing.Any
     "The underlying tensordict."
 
-    def invoke(self, module: nn.Module) -> typing.Any:
+    def __post_init__(self) -> None:
+        _ = self.invoke
+
+    @property
+    def invoke(self) -> Invoker:
         "Invoke the module for you, with the unpacking taken care of."
 
-        args, kwargs = self.apply(type(module))
+        if isinstance(self.arguments, cabc.Mapping | td.TensorDict | td.TensorClass):
+            return self._invoke_tdict
+
+        if isinstance(self.arguments, torch.Tensor):
+            return self._invoke_tensor
+
+        raise TypeError(f"Unhandled type {type(self.argument)=}.")
+
+    def _invoke_tensor(self, module: nn.Module):
+        return module(self.arguments)
+
+    def _invoke_tdict(self, module: nn.Module) -> typing.Any:
+        args, kwargs = self._apply_tdict(type(module))
         return module(*args, **kwargs)
 
-    def apply(
+    def _apply_tdict(
         self, nn_type: type[nn.Module]
     ) -> tuple[list[typing.Any], dict[str, typing.Any]]:
         "Apply `self` onto `nn.Module.forward` of the given `nn_type`."
@@ -43,9 +64,9 @@ class NnArgs:
 
         for key, param in signature.params.items():
             if param.is_positional_only:
-                args.append(self.tdict[key])
+                args.append(self.arguments[key])
 
             else:
-                kwargs[key] = self.tdict[key]
+                kwargs[key] = self.arguments[key]
 
         return args, kwargs
