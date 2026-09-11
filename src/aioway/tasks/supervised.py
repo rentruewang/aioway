34eb27d6d387e31a8d6664416651105f8f1df5dc
RenChu Wang <patrick1031wang@gmail.com>
2026-09-10 19:55:44 -0700

    update

--- src/aioway/tasks/supervised.py
+++ src/aioway/tasks/supervised.py
@@ -1,1 +1,17 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
+
+import typing
+from .tasks import Task, NnInput, BatchIter
+
+
+class SupervisedTask[T: NnInput](Task[T]):
+    @typing.override
+    def fake(self) -> T:
+        raise NotImplementedError
+
+    @typing.override
+    def iterator(self) -> BatchIter[T]:
+        raise NotImplementedError
+
+    def step(self, batch: T) -> None:
+        raise NotImplementedError
