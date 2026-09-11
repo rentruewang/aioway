4ebf0f77e0d52535175dade61aff114f4578aa5f
RenChu Wang <patrick1031wang@gmail.com>
2026-09-10 20:14:57 -0700

    update

--- src/aioway/nn/rewrites/rewrites.py
+++ src/aioway/nn/rewrites/rewrites.py
@@ -17,8 +17,25 @@ class Rewriter[I: nn.Module = typing.Any, O: nn.Module = typing.Any](abc.ABC):
 
     @typing.no_type_check
     def __call__(self, module: nn.Module) -> nn.Module:
+        if not self.handle(module):
+            return NotImplemented
+
         return self.rewrite(module)
 
     @abc.abstractmethod
+    def handle(self, module: I, /) -> bool:
+        """
+        Check whether the `Rewriter` handles the module or not.
+        """
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
     def rewrite(self, module: I, /) -> O:
+        """
+        Perform the rewrite. Should not modify the input module.
+
+        This shall not raise an exception, as it assumes input is valid.
+        """
+
         raise NotImplementedError
