ce6d4c52b2e2e604b155eca5739b3b5d4b2ac034
RenChu Wang <patrick1031wang@gmail.com>
2026-09-19 20:26:00 -0700

    fmt#

--- src/aioway/torch/attrs/caches.py
+++ src/aioway/torch/attrs/caches.py
@@ -7,8 +7,8 @@ from collections import abc as cabc
 import tensordict as td
 import torch
 
-from aioway.torch.overrides import is_real
 from aioway.torch._utils import tcol_to_tdict
+from aioway.torch.overrides import is_real
 
 from .attrs import Attr
 from .schemas import Schema
