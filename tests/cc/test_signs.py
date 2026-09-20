b0bdd0acdd1fae844aeab2b09864096e795c1375
RenChu Wang <patrick1031wang@gmail.com>
2026-09-19 21:06:13 -0700

    losses

--- tests/cc/test_signs.py
+++ tests/cc/test_signs.py
@@ -12,43 +12,56 @@ def _module_type_signs():
     LOSS_SIGN = Sign.from_inputs("input", "target")
     BILINAR_SIGN = Sign.from_inputs("input1", "input2")
 
-    yield nn.AvgPool1d, LAYER_SIGN
-    yield nn.AvgPool2d, LAYER_SIGN
-    yield nn.AvgPool3d, LAYER_SIGN
-    yield nn.BatchNorm1d, LAYER_SIGN
-    yield nn.BatchNorm2d, LAYER_SIGN
-    yield nn.BatchNorm3d, LAYER_SIGN
-    yield nn.CELU, LAYER_SIGN
-    yield nn.Conv1d, LAYER_SIGN
-    yield nn.Conv2d, LAYER_SIGN
-    yield nn.Conv3d, LAYER_SIGN
-    yield nn.Dropout1d, LAYER_SIGN
-    yield nn.Dropout2d, LAYER_SIGN
-    yield nn.Dropout3d, LAYER_SIGN
-    yield nn.Embedding, LAYER_SIGN
-    yield nn.Flatten, LAYER_SIGN
-    yield nn.GELU, LAYER_SIGN
-    yield nn.Identity, LAYER_SIGN
-    yield nn.InstanceNorm1d, LAYER_SIGN
-    yield nn.InstanceNorm2d, LAYER_SIGN
-    yield nn.InstanceNorm3d, LAYER_SIGN
-    yield nn.LayerNorm, LAYER_SIGN
-    yield nn.Linear, LAYER_SIGN
-    yield nn.MaxPool1d, LAYER_SIGN
-    yield nn.MaxPool2d, LAYER_SIGN
-    yield nn.MaxPool3d, LAYER_SIGN
-    yield nn.ReLU, LAYER_SIGN
-    yield nn.ReLU6, LAYER_SIGN
-    yield nn.Sequential, LAYER_SIGN
-    yield nn.Sigmoid, LAYER_SIGN
-    yield nn.Tanh, LAYER_SIGN
-    yield nn.Unflatten, LAYER_SIGN
+    for layer in _layers():
+        yield layer, LAYER_SIGN
 
-    yield nn.L1Loss, LOSS_SIGN
-    yield nn.SmoothL1Loss, LOSS_SIGN
-    yield nn.MSELoss, LOSS_SIGN
+    for loss in _losses():
+        yield loss, LOSS_SIGN
 
     yield nn.Bilinear, BILINAR_SIGN
+
+
+def _losses():
+    yield nn.L1Loss()
+    yield nn.SmoothL1Loss()
+    yield nn.MSELoss()
+    yield nn.CrossEntropyLoss()
+    yield nn.BCEWithLogitsLoss()
+    yield nn.BCELoss()
+
+
+def _layers():
+    yield nn.AvgPool1d
+    yield nn.AvgPool2d
+    yield nn.AvgPool3d
+    yield nn.BatchNorm1d
+    yield nn.BatchNorm2d
+    yield nn.BatchNorm3d
+    yield nn.CELU
+    yield nn.Conv1d
+    yield nn.Conv2d
+    yield nn.Conv3d
+    yield nn.Dropout1d
+    yield nn.Dropout2d
+    yield nn.Dropout3d
+    yield nn.Embedding
+    yield nn.Flatten
+    yield nn.GELU
+    yield nn.Identity
+    yield nn.InstanceNorm1d
+    yield nn.InstanceNorm2d
+    yield nn.InstanceNorm3d
+    yield nn.LayerNorm
+    yield nn.Linear
+    yield nn.MaxPool1d
+    yield nn.MaxPool2d
+    yield nn.MaxPool3d
+    yield nn.ReLU
+    yield nn.ReLU6
+    yield nn.Sequential
+    yield nn.Sigmoid
+    yield nn.Tanh
+    yield nn.Unflatten
 
 
 @pytest.mark.parametrize("module_type,sign", _module_type_signs())
