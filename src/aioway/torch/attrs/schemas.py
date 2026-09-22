14a0c809aba695b5474b5a8ef07c56ef38d17cf1
RenChu Wang <patrick1031wang@gmail.com>
2026-09-21 18:18:05 -0700

    recurse

--- src/aioway/torch/attrs/schemas.py
+++ src/aioway/torch/attrs/schemas.py
@@ -1,13 +1,13 @@
 # Copyright (c) AIoWay Authors - All Rights Reserved
 
-import collections
 import json
 import typing
 from collections import abc as cabc
 
+import loguru as L
 import tensordict as td
-import torch
 
+from aioway._utils import is_tuple_of
 from aioway.torch.matches import TorchMatcher
 
 from .attrs import Attr
@@ -16,16 +16,83 @@ from .dtypes import DType
 __all__ = ["Schema"]
 
 
-class Schema(collections.UserDict[str, "Attr | Schema"]):
+class Schema(cabc.Mapping):
     """
     `Schema` is a `dict[str, Attr]` with additional utilities.
     """
+
+    def __init__(self, mapping: dict[str, typing.Any] | None = None) -> None:
+        self._schemas = mapping or {}
+
+    def __len__(self) -> int:
+        return len(self._schemas)
+
+    def __getitem__(self, key: str | tuple[str, ...], /) -> Attr | Schema:
+        if isinstance(key, str):
+            return self._schemas[key]
+
+        try:
+            return self._getitem_recurse(*key)
+        except KeyError, AssertionError:
+            raise KeyError(key)
 
     def __getstate__(self) -> dict[str, typing.Any]:
         return {key: val.__getstate__() for key, val in self.items()}
 
     def __hash__(self) -> int:
         return hash(json.dumps(self.__getstate__(), sort_keys=True))
+
+    @typing.no_type_check
+    def __iter__(self) -> cabc.Generator[str]:
+        yield from self._keys()
+
+    def _getitem_recurse(self, *key: str):
+        assert key
+
+        first, *rest = key
+
+        try:
+            child = self[first]
+        except KeyError:
+            raise KeyError
+
+        # Still more to recurse. Must be `Schema`.
+        if rest:
+            assert isinstance(child, Schema)
+            return child._getitem_recurse(*rest)
+
+        # If it's tuple of 1 level (only first), we have reached the end.
+        else:
+            assert isinstance(child, Attr)
+            return child
+
+    def _keys(self, *, leaves_only: bool = False, include_nested: bool = False):
+        if include_nested and leaves_only:
+            raise ValueError("`leaves_only` and `include_nested` cannot both be true.")
+
+        # Always yield the non nested keys.
+        for key, val in self._items_of_type(Attr):
+            yield key
+
+        if leaves_only:
+            return
+
+        if not include_nested:
+            for key, _ in self._items_of_type(Schema):
+                yield key
+
+        # This is the branch where it would yield sub-schema keys as tuples.
+        L.logger.trace("Recurse into sub-Schema of {} and yield tuples", self)
+        for key, val in self._items_of_type(Schema):
+            for child_key in val._keys(include_nested=True):
+                child_key = child_key if isinstance(key, tuple) else (child_key,)
+                assert is_tuple_of(str)(child_key)
+                yield key, *child_key
+
+    def _items_of_type[T](self, typ: type[T]) -> cabc.Generator[tuple[str, T]]:
+        for key, child in self._schemas.items():
+            if isinstance(child, typ):
+                yield key, child
 
     @property
     def dtype(self) -> DType | None:
